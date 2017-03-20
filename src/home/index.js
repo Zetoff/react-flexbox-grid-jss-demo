e548a885f4ff16799e0c24eebfdc8633710eef23
PolGuixe <pol@zetoff.com>
2017-03-19 20:40:58 -0400

    eliminated default boilerplate
    added tests

--- src/home/index.js
+++ src/home/index.js
@@ -9,43 +9,31 @@
  */
 
 import React, { PropTypes } from 'react';
-import Layout from '../../components/Layout';
-import s from './styles.css';
-import { title, html } from './index.md';
+import {
+  Container,
+  ContainerFluid,
+  Row,
+} from 'react-flexbox-grid-jss';
 
 class HomePage extends React.Component {
 
   static propTypes = {
-    articles: PropTypes.arrayOf(PropTypes.shape({
-      url: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-    }).isRequired).isRequired,
   };
-
-  componentDidMount() {
-    document.title = title;
-  }
 
   render() {
     return (
-      <Layout className={s.content}>
-        <div
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        <h4>Articles</h4>
-        <ul>
-          {this.props.articles.map(article =>
-            <li key={article.url}>
-              <a href={article.url}>{article.title}</a>
-            by {article.author}</li>,
-          )}
-        </ul>
-        <p>
-          <br /><br />
-        </p>
-      </Layout>
+      <div>
+        <Container>
+          <Row>
+          Hello 1
+        </Row>
+        </Container>
+        <ContainerFluid>
+          <Row>
+          Hello 2
+        </Row>
+        </ContainerFluid>
+      </div>
     );
   }
 
